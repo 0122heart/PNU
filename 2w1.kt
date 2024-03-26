2e87261e2ec6683a7bdc67f761f8edbfc7ca7e9d
0122heart <145850192+0122heart@users.noreply.github.com>
2024-03-26 16:59:31 +0900

    Add files via upload

---
+++ 2w1.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+fun main(){
+
+    val a = readLine()
+    println("입력 받을 숫자: ${a!!}")
+
+    for(i in 1..a.toInt()){
+        if(i % 2 == 1){
+            print("$i ")
+        }
+    }
+}
